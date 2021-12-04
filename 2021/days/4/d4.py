a8ce90ee0e01198a4f1f714abbda3e7e9063c50b
Pieter Vanderpol <pjvanderpol@gmail.com>
2021-12-04 11:50:30 -0800

    refactor day 4

--- 2021/days/4/d4.py
+++ 2021/days/4/d4.py
@@ -1,73 +1,51 @@
 # %%
-import re
 import numpy as np
+
 
 def parse_input(path="./input"):
     with open(path) as f:
         text = f.read().strip()
-    
 
-    lines = text.split("\n")
-    numbers = [*map(int, lines[0].strip().split(","))]
-    boards_text = "\n".join(lines[2:]).strip().split("\n\n")
-    
-    boards = [parse_board(b) for b in boards_text]
-    return numbers,boards
+    numbers, *boards = [l for l in text.split("\n\n")]
+    numbers = [np.int64(n) for n in numbers.strip().split(",")]
+    boards = [parse_board(b) for b in boards]
+    return numbers, boards
+
 
 def parse_board(board_text):
-    return np.array([parse_board_line(row) for row in board_text.split("\n")])
+    return np.array([parse_board_row(n) for n in board_text.split("\n")])
 
-def parse_board_line(line: str):
-    line = line.strip()
-    line = re.sub("\s+", " ", line)
-    return tuple(map(int, line.split(" ")))
+
+def parse_board_row(row: str):
+    return tuple(map(int, row.split()))
+
+
+def get_num_coords(number, board):
+    return tuple(zip(*np.where(board == number)))
+
 
 numbers, boards = parse_input("input")
 
 
-
-
-def get_num_coords(number, board):
-    return tuple(zip(*np.where(board==number)))
-    
-
-def find_final_score():
+def gen_winning_scores():
     seen = set()
-    for i, number in map(np.int64, numbers):
-        seen.add(number)
-        for board in boards:
-            coords = get_num_coords(number, board)
-            for coord in coords:
-                is_winning_row = all(n in seen for n in  board[coord[0], :])
-                is_winning_col = all(n in seen for n in  board[:, coord[1]])
-                if is_winning_row or is_winning_col:
-                    seen_inv_masked_sum = sum(n for n in board.flatten() if not n in seen)
-                    return seen_inv_masked_sum * number                
-    
+    winning_boards = set()
 
-print(boards[0])
-find_final_score()
-# %% p2
-
-def find_last_board_win_final_score():
-    seen = set()
-    winning_already = set()
-    board_count = len(boards)
-    
-    i_boards = list(enumerate(boards))
     for number in map(np.int64, numbers):
         seen.add(number)
-        for b_index, board in filter(lambda e: not e[0] in winning_already, i_boards):
-            print("trying ", b_index)
-            coords = get_num_coords(number, board)
-            for coord in coords:
-                is_winning_row = all(n in seen for n in  board[coord[0], :])
-                is_winning_col = all(n in seen for n in  board[:, coord[1]])
+        for b_index, board in enumerate(boards):
+            if b_index in winning_boards:
+                continue
+            for coord in get_num_coords(number, board):
+                is_winning_row = all(n in seen for n in board[coord[0], :])
+                is_winning_col = all(n in seen for n in board[:, coord[1]])
                 if is_winning_row or is_winning_col:
-                    print(b_index, "won!")
-                    winning_already.add(b_index)
-                    if len(winning_already) >= board_count:
-                        seen_inv_masked_sum = sum(n for n in board.flatten() if not n in seen)
-                        return seen_inv_masked_sum * number
+                    winning_boards.add(b_index)
+                    seen_inv_masked_sum = sum(
+                        n for n in board.flatten() if not n in seen)
+                    yield seen_inv_masked_sum * number
 
-find_last_board_win_final_score()
+
+scores = [*gen_winning_scores()]
+print("p1: ", scores[0])
+print("p2: ", scores[-1])
