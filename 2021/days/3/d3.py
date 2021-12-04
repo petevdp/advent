f5dbb94fbcd9db47c0c2daa70fb8b0565cc1a37b
Pieter Vanderpol <pjvanderpol@gmail.com>
2021-12-03 22:29:39 -0800

    refactor day 3

--- 2021/days/3/d3.py
+++ 2021/days/3/d3.py
@@ -1,6 +1,5 @@
 # %% p1
 
-import numpy as np
 import math
 
 def parse_inputs(path="./input"):
@@ -9,15 +8,14 @@ def parse_inputs(path="./input"):
         return [[*map(int, list(l))] for l in lines]
 
 
-I = parse_inputs()
 
-def get_position_mask_gamma(bits):
+def get_majority_value(bits):
     half = math.ceil(len(bits) / 2)
     sum_digit = sum(bits)
     gamma = int(sum_digit >= half)
     return bool(gamma)
 
-def get_position_mask_epsilon(bits):
+def get_minority_value(bits):
     half = math.ceil(len(bits) / 2)
     sum_digit = sum(bits)
     epsilon = int(sum_digit < half)
@@ -27,14 +25,13 @@ def get_position_mask_epsilon(bits):
 def b_to_int(b_arr):
     return sum([d* 2**i for i, d in enumerate(reversed(b_arr))])
 
-
-gamma_bits = [get_position_mask_gamma(bits) for bits in zip(*I)]
+I = parse_inputs()
+gamma_bits = [get_majority_value(bits) for bits in zip(*I)]
 epsilon_bits = [not b for b in gamma_bits]
 
 print("p1: ", b_to_int(gamma_bits) * b_to_int(epsilon_bits))
 
 # %% p2
-
 
 def get_rating(get_mask, nums):
     remaining = nums
@@ -42,16 +39,14 @@ def get_rating(get_mask, nums):
     while len(remaining) > 1:
         bits = [d[digit_place] for d in remaining]
         mask = get_mask(bits)
-        with_mask = [(b, b[digit_place] == mask) for b in remaining]
         remaining = [b for b in remaining if b[digit_place] == mask]
         digit_place += 1
     
     return b_to_int(remaining[0])
 
 
-oxygen_rating = get_rating(get_position_mask_gamma, I)
-co2_rating = get_rating(get_position_mask_epsilon, I)
+oxygen_rating = get_rating(get_majority_value, I)
+co2_rating = get_rating(get_minority_value, I)
 
 
 print("p2: ", oxygen_rating * co2_rating)
-# print("p2: ", oxygen_rating * co2_rating)
