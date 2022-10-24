288131d77a989e24ec8b53dd9516e03f8525eabf
petevdp <pjvanderpol@gmail.com>
2022-10-23 21:45:47 -0700

    working p1

--- 2021/days/16/d15.py
+++ 2021/days/16/d15.py
@@ -31,37 +31,41 @@ class OperatorPacket(NamedTuple):
 str_rep = None
 pointer = 0
 
+with open('./input') as f:
+    text = f.read().strip()
+
 examples = [
-    '8A004A801A8002F478',
-    '620080001611562C8802118E34',
-    'C0015000016115A2E0802F182340',
-    'A0016C880162017C3686B18A3D4780',
+    text,
+    # '8A004A801A8002F478',
+    # '620080001611562C8802118E34',
+    # 'C0015000016115A2E0802F182340',
+    # 'A0016C880162017C3686B18A3D4780',
 ]
 
 
 def main():
     global str_rep, pointer
-    data = parse_input()
-    sum_all_versions(data)
-
-
-def run_examples():
     for example in examples:
-        print(example)
-        bin_examples = bin(int(example, 16))[2:]
-        sum_all_versions(bin_examples)
+        pointer = 0
+        str_rep = bin(int(example, 16))[2:].zfill(len(example) * 4)
 
+        oft_disp = ''
+        hex_disp = ''
+        bin_disp = ''
+        for i in range(0, len(example)):
+            oft_disp += str(i).ljust(4, ' ') + ' '
+            hex_disp += example[i].ljust(4, ' ') + ' '
+            bin_disp += str_rep[i * 4: i * 4 + 4] + ' '
+        print(oft_disp)
+        print(hex_disp)
+        print(bin_disp)
 
-def sum_all_versions(data):
-    global pointer, str_rep
-    print(data)
-    pointer = 0
-    str_rep = data
-    root_packet = parse_packet()
-    packets = [p for p in yield_all_packets(root_packet)]
-    sum_versions = sum(p.header.version for p in packets)
-    print(sum_versions)
-    print()
+        root_packet = parse_packet()
+        print(root_packet)
+        packets = [p for p in yield_all_packets(root_packet)]
+        sum_versions = sum(p.header.version for p in packets)
+        print(sum_versions)
+        print()
 
 
 def yield_all_packets(packet):
@@ -99,6 +103,7 @@ def parse_packet():
         while cont == '0b1':
             cont = parse_segment(Segment(1, as_bin))
             sub_bits += parse_segment(Segment(4, as_str))
+        # pass_leftovers(packet_start_pointer)
         number = bin_as_int(sub_bits)
         return LiteralPacket(header, number)
 
@@ -117,6 +122,13 @@ def parse_packet():
     return OperatorPacket(header, length_type_id, sub_packet_length, sub_packets)
 
 
+def pass_leftovers(starting_pointer):
+    global pointer
+    hex_leftovers = (4 - ((pointer - starting_pointer) % 4)) % 4
+    pointer += hex_leftovers
+    print(pointer)
+
+
 def parse_segment(segment):
     global pointer
     str_segment = str_rep[pointer:segment.len + pointer]
@@ -125,11 +137,39 @@ def parse_segment(segment):
     return value
 
 
+def test():
+    inputA = int('0101', 2)
+    print("Before shifting " + str(inputA) + " " + bin(inputA))
+    print("After shifting in binary: " + bin(inputA << 1))
+    print("After shifting in decimal: " + str(inputA << 1))
+
+
+conv_table = {
+    "0": "0000",
+    "1": "0001",
+    "2": "0010",
+    "3": "0011",
+    "4": "0100",
+    "5": "0101",
+    "6": "0110",
+    "7": "0111",
+    "8": "1000",
+    "9": "1001",
+    "A": "1010",
+    "B": "1011",
+    "C": "1100",
+    "D": "1101",
+    "E": "1110",
+    "F": "1111"
+}
+
+
 def parse_input(path='./input'):
+    return example3_operator
     with open(path) as f:
-        text = f.read().strip()
-    return bin(int(text, 16))[2:]
+        text = f.read()
+    return str(bin(int(example1, 16)))[2:]
 
 
 if __name__ == '__main__':
-    run_examples()
+    main()
