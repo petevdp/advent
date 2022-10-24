ae18df198c46d6ce955811a2e54fe186c4cb03a9
petevdp <pjvanderpol@gmail.com>
2022-10-23 22:15:25 -0700

    complete d16

--- 2021/days/16/d16.py
+++ 2021/days/16/d16.py
@@ -1,9 +1,17 @@
+import math
 from enum import Enum
 from typing import NamedTuple, Dict, List
 
 
 class PacketType(Enum):
+    SUM = 0
+    PRODUCT = 1
+    MINIMUM = 2
+    MAXIMUM = 3
     LITERAL = 4
+    GREATER_THAN = 5
+    LESS_THAN = 6
+    EQUAL_TO = 7
 
 
 class Segment(NamedTuple):
@@ -18,7 +26,7 @@ class Header(NamedTuple):
 
 class LiteralPacket(NamedTuple):
     header: Header
-    number: int
+    value: int
 
 
 class OperatorPacket(NamedTuple):
@@ -26,46 +34,56 @@ class OperatorPacket(NamedTuple):
     length_type: bin
     length: int
     sub_packets: List[any]
+    value: int
 
 
 str_rep = None
 pointer = 0
 
-with open('./input') as f:
-    text = f.read().strip()
-
 examples = [
-    text,
-    # '8A004A801A8002F478',
-    # '620080001611562C8802118E34',
-    # 'C0015000016115A2E0802F182340',
-    # 'A0016C880162017C3686B18A3D4780',
+    'C200B40A82',
+    '04005AC33890',
+    '880086C3E88112',
+    'CE00C43D881120',
+    'D8005AC2A8F0',
+    'F600BC2D8F',
+    '9C005AC2F8F0',
+    '9C0141080250320F1802104A08',
 ]
 
 
 def main():
     global str_rep, pointer
-    example = parse_input()
+    data = parse_input()
 
     pointer = 0
-    str_rep = example
+    str_rep = data
 
     oft_disp = ''
     hex_disp = ''
     bin_disp = ''
-    for i in range(0, len(example)):
+    for i in range(0, len(data)):
         oft_disp += str(i).ljust(4, ' ') + ' '
-        hex_disp += example[i].ljust(4, ' ') + ' '
+        hex_disp += data[i].ljust(4, ' ') + ' '
         bin_disp += str_rep[i * 4: i * 4 + 4] + ' '
     print(oft_disp)
     print(hex_disp)
     print(bin_disp)
 
     root_packet = parse_packet()
-    packets = [p for p in yield_all_packets(root_packet)]
-    sum_versions = sum(p.header.version for p in packets)
-    print(sum_versions)
+    print(f'value: {root_packet.value}')
     print()
+
+
+def run_examples():
+    global str_rep, pointer
+    for example in examples:
+        print(example)
+        pointer = 0
+        str_rep = parse_hex(example)
+        root_packet = parse_packet()
+        print(root_packet.value)
+        print()
 
 
 def yield_all_packets(packet):
@@ -103,7 +121,6 @@ def parse_packet():
         while cont == '0b1':
             cont = parse_segment(Segment(1, as_bin))
             sub_bits += parse_segment(Segment(4, as_str))
-        # pass_leftovers(packet_start_pointer)
         number = bin_as_int(sub_bits)
         return LiteralPacket(header, number)
 
@@ -119,14 +136,33 @@ def parse_packet():
         sub_packet_length = parse_segment(Segment(11, bin_as_int))
         for p_idx in range(sub_packet_length):
             sub_packets.append(parse_packet())
-    return OperatorPacket(header, length_type_id, sub_packet_length, sub_packets)
 
+    sub_vals = [sp.value for sp in sub_packets]
+    match header.type_id:
+        case PacketType.SUM.value:
+            value = sum(sub_vals)
+        case PacketType.PRODUCT.value:
+            value = math.prod(sub_vals)
+        case PacketType.MINIMUM.value:
+            value = min(sub_vals)
+        case PacketType.MAXIMUM.value:
+            value = max(sub_vals)
+        case PacketType.GREATER_THAN.value:
+            val1 = sub_packets[0].value
+            val2 = sub_packets[1].value
+            value = int(val1 > val2)
+        case PacketType.LESS_THAN.value:
+            val1 = sub_packets[0].value
+            val2 = sub_packets[1].value
+            value = int(val1 < val2)
+        case PacketType.EQUAL_TO.value:
+            val1 = sub_packets[0].value
+            val2 = sub_packets[1].value
+            value = int(val1 == val2)
+        case _:
+            raise Exception(f"unhandled packet type {header.type_id}")
 
-def pass_leftovers(starting_pointer):
-    global pointer
-    hex_leftovers = (4 - ((pointer - starting_pointer) % 4)) % 4
-    pointer += hex_leftovers
-    print(pointer)
+    return OperatorPacket(header, length_type_id, sub_packet_length, sub_packets, value)
 
 
 def parse_segment(segment):
@@ -137,11 +173,15 @@ def parse_segment(segment):
     return value
 
 
+def parse_hex(text):
+    return bin(int(text, 16))[2:].zfill(len(text) * 4)
+
+
 def parse_input(path='./input'):
     with open(path) as f:
         text = f.read().strip()
 
-    return bin(int(text, 16))[2:].zfill(len(text) * 4)
+    return parse_hex(text)
 
 
 if __name__ == '__main__':
