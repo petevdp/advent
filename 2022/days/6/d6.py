2a997ddbeaf192046ac352bdda3a28b4caacca0b
petevdp <pjvanderpol@gmail.com>
2022-12-05 22:05:36 -0800

    make solution less dumb

--- 2022/days/6/d6.py
+++ 2022/days/6/d6.py
@@ -5,7 +5,7 @@ with open('input') as f:
 def earliest_n_unique(n: int):
     for i in range(len(stream)):
         cl = stream[i:i + n]
-        if len(cl) == len(set(cl)):
+        if n == len(set(cl)):
             return i + n
     raise "no match found"
 
