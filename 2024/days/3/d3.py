87fba0f417401f3ce8481eecca06b0c2a69de189
petevdp <pjvanderpol@gmail.com>
2024-12-04 10:16:54 -0800

    clean up d3

--- 2024/days/3/d3.py
+++ 2024/days/3/d3.py
@@ -5,21 +5,20 @@ with open(sys.argv[1]) as f:
     T = f.read()
 
 def solve():
-    matches = re.findall(r"((mul)|(do)|(don't))\(((\d+),(\d+))?\)", T)
+    matches = re.findall(r"(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))", T)
     p1 = 0
     p2 = 0
     do = True
     for m in matches:
-        if m[0] == "do":
+        if m[-2] == "do()":
             do = True
             continue
-        if m[0] == "don't":
+        if m[-1] == "don't()":
             do = False
             continue
-        a,b = map(int, m[-2:])
+        a,b = map(int, m[1:3])
         p1 += a * b
-        if not do: continue
-        p2 += a * b
+        if do: p2 += a * b
     print("p1", p1)
     print("p2", p2)
 
